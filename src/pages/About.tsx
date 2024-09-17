9de4e817024bd607e121f2fabef8c644dbca4808
abhijeet141 <sinhaabhijeet.smith@gmail.com>
2024-09-17 22:03:36 +0530

    About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,20 @@
 export function About(){
     return(
-        <div>
-        AboutPage
+        <div className="px-12 xl:container mx-auto">
+            <div className="flex flex-col">
+                <div className="mb-6 text-2xl">
+                <h3>About Me</h3>
+                </div>
+                <div className="text-left text-justify">
+                <p className="mb-9 text-black/80">
+                Abhijeet Sinha is a recent graduate with a B.Tech in  Computer Science Engineering from Lovely Professional University, with a strong passion for Full Stack Web Development. He is known for his flexibility and adaptability in various situations.
+                Abhijeet has a solid foundation in JavaScript, React, Express, and Problem Solving, and is known for developing efficient and user-friendly web applications. 
+                <br /><br />In addition to his technical skills, Abhijeet has proven his presentation skills as a co-author of a research paper on Comparative Analysis of Anomaly Based Intrusion Detection System on Artificial Intelligence, which he presented at the NIELIT International Conference 2023 published by Springer.
+                <br /><br />Abhijeet has also contributed significantly to the open-source community. As a Project Admin for GirlScript Summer Of Code, he administered the CropForesight project, providing constructive feedback to contributors for code improvements. He was also recognized as a top performer and contributed to MakesMathEasy by adding various features using JavaScript and enhancing the UI using CSS.
+                <br /><br />Abhijeet is now looking for opportunities to apply his skills and knowledge in a challenging environment where he can make a significant impact. He is particularly interested in roles that would allow him to further develop his skills in Full stack Web Development. He is open to both internships and full-time roles. Feel free to reach out to him if you have any opportunities that might be a good fit. He is excited about the possibility of bringing his unique blend of skills and experience to your team.
+                </p>
+                </div>
+            </div>
         </div>
     )
 }

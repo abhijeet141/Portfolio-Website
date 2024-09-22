40fc9b6f11d5dac4b56debf205b09e75a515b7c0
abhijeet141 <sinhaabhijeet.smith@gmail.com>
2024-09-22 20:46:38 +0530

    adjustment to website layout

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -9,18 +9,28 @@ type SocialsProps = {
 export function Socials({iconstyles}: SocialsProps){
     return(
         <div className="flex gap-6">
-        <a href="https://github.com/abhijeet141" className={iconstyles} target="_blank" rel="noopener noreferrer">
-        <FaGithub />
-        </a>
-        <a href="https://www.linkedin.com/in/abhijeet-sinha264/" className={iconstyles} target="_blank" rel="noopener noreferrer">
-        <FaLinkedin />
-        </a>
-        <a href="https://x.com/Abhijee64915105" className={iconstyles} target="_blank" rel="noopener noreferrer">
-        <FaTwitter/>
-        </a>
-        <a href="https://leetcode.com/u/abhijeet264/" className={iconstyles} target="_blank" rel="noopener noreferrer">
-        <SiLeetcode/>
-        </a>     
+            <div>
+                <a href="https://github.com/abhijeet141" className={iconstyles} target="_blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </a>
+            </div>
+
+            <div className="text-[#007acc]">
+                <a href="https://www.linkedin.com/in/abhijeet-sinha264/" className={iconstyles} target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+            </div>
+
+            <div className="text-[#1DA1F2]">
+                <a href="https://x.com/Abhijee64915105" className={iconstyles} target="_blank" rel="noopener noreferrer">
+                    <FaTwitter/>
+                </a>
+            </div>
+            <div className="text-[#333333]">
+                <a href="https://leetcode.com/u/abhijeet264/" className={iconstyles} target="_blank" rel="noopener noreferrer">
+                    <SiLeetcode/>
+                </a>   
+            </div>  
         </div>
     )
 }
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -27,7 +27,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center bg-custom-orange text-white w-full justify-center whitespace-nowrap rounded-lg p-3 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-red-orange data-[state=active]:text-slate-950 data-[state=active]:shadow-sm",
+      "inline-flex items-center bg-custom-orange text-white w-full justify-center whitespace-nowrap rounded-lg p-3 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-red-orange",
       className
     )}
     {...props}
